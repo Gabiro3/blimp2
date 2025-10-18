f349ed43cc8483762b7f5547e09f605282eb84d9
Gabiro Arnaud <gabiroarnaud@gmail.com>
2025-10-18 09:51:51 +0200

    fhfjhd

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,9 @@
 """Utils package for inter-app connector functions."""
 
-from .gmail_calendar_utils import emails_to_calendar_events as get_all_msgs_to_events
-from .gmail_gdrive_utils import save_attachments_to_drive as save_all_attachments_to_drive
+from .gmail_calendar_utils import GmailCalendarUtils
+from .gmail_gdrive_utils import GmailGDriveUtils
 
 __all__ = [
-    "get_all_msgs_to_events",
-    "save_all_attachments_to_drive",
+    "GmailCalendarUtils",
+    "GmailGDriveUtils",
 ]
